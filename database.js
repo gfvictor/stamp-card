f0b852485a5cd649b6656203abce087f960d7f34
Victor Farias <vfarias@outlook.com>
2024-11-28 11:09:20 +0900

    feat: defined database connection - created database.js - renamed server.js back to app.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://gfvictor:admin@stamp-card-cluster.2vsjw.mongodb.net/?retryWrites=true&w=majority&appName=stamp-card-cluster";
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(uri, {
+            serverApi: {
+                deprecationErrors: true,
+                strict: true,
+                version: '1',
+            }
+        });
+        await mongoose.connection.db.admin().command({ping: 1});
+    } catch (err) {
+        console.error('Erro ao conectar com MongoDB:', err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
