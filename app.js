f882a412c9fecab8a88cf6329ac7e3acfcae926e
Victor Farias <vfarias@outlook.com>
2024-12-02 09:57:17 +0900

    refactor: added Point routes to app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const connectDB = require('./database')
 
 const clientRoutes = require('./routes/clientRoutes');
 const storeRoutes = require('./routes/storeRoutes');
+const transactionsRoutes = require('./routes/transactionsRoutes');
+const pointsRoutes = require('./routes/pointsRoutes');
 
 const app = express();
 
@@ -24,6 +26,8 @@ app.use(express.urlencoded({ extended: false }));
 // api routes reserved space lol
 app.use('/api/clients', clientRoutes);
 app.use('/api/stores', storeRoutes);
+app.use('/api/transaction', transactionsRoutes);
+app.use('/api/points', pointsRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({error: 'Not found'});
