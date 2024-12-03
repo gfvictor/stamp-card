ccab97f7c5c2b5039ef93c82099061fd9af351ee
Victor Farias <vfarias@outlook.com>
2024-12-03 10:02:20 +0900

    refactor: changed the database uri and added timeout

--- database.js
+++ database.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://encode-test:encode@stamp-card-cluster.2vsjw.mongodb.net/?retryWrites=true&w=majority&appName=stamp-card-cluster";
+const uri = "mongodb+srv://encode-test:encode@stamp-card-cluster.2vsjw.mongodb.net/point_card?retryWrites=true&w=majority&appName=stamp-card-cluster";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(uri, {
-            serverApi: {
-                deprecationErrors: true,
-                strict: true,
-                version: '1',
-            }
-        });
-        await mongoose.connection.db.admin().command({ping: 1});
+        await mongoose.connect(uri, {serverSelectionTimeoutMS: 5000});
     } catch (err) {
         console.error('Erro ao conectar com MongoDB:', err);
         process.exit(1);
