c7c8188cc15738e407ce4ea026915d2d6b823aeb
Victor Farias <vfarias@outlook.com>
2024-12-04 12:17:59 +0900

    refactor: delegated logic to controller

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,66 +1,12 @@
 const express = require('express');
-const Client = require('../models/client');
+const clientController = require('../controllers/clientController');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-        const newClient = new Client(req.body);
-        const savedClient = await newClient.save();
-        res.status(201).json(savedClient);
+router.post('/', (req, res) => clientController.createClient(req, res));
+router.get('/', (req, res) => clientController.getAllClients(req, res));
+router.get('/:id', (req, res) => clientController.getClientById(req, res));
+router.put('/:id', (req, res) => clientController.updateClient(req, res));
+router.delete('/:id', (req, res) => clientController.deleteClient(req, res));
 
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-router.get('/', async (req, res) => {
-    try {
-        const clients = await Client.find();
-        res.status(200).json(clients);
-
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-        const client = await Client.findById(req.params.id);
-
-        client
-        ? res.status(200).json(client)
-        : res.status(404).json({message: "Client not found."});
-
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-})
-
-router.put('/:id', async (req,res) => {
-    try {
-        const updatedClient = await Client.findByIdAndUpdate(req.params.id, req.body, {new: true});
-
-        updatedClient
-        ? res.status(200).json(updatedClient)
-        : res.status(404).json({message: "Client not found."});
-
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const deletedClient = await Client.findByIdAndDelete(req.params.id);
-
-        deletedClient
-        ? res.status(200).json({message: "Client successfully deleted."})
-        : res.status(404).json({message: "Client not found."});
-
-    } catch (err) {
-      res.status(500).json({error: err.message});
-    }
-});
-
-module.exports = router
+module.exports = router;
