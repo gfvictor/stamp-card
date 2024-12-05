a431739214563b30a03341c24cf1f41515c32293
Victor Farias <vfarias@outlook.com>
2024-12-05 11:24:32 +0900

    refactor: delegated logic to controller

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,66 +1,16 @@
 const express = require('express');
-const Store = require('../models/store');
+const storeController = require('../controllers/storeController');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-        const newStore = new Store(req.body);
-        const savedStore = await newStore.save();
-        res.status(201).json(savedStore);
+router.post('/', async (req, res) => storeController.createStore(req, res));
 
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
+router.get('/', async (req, res) => storeController.getAllStores(req, res));
 
-router.get('/', async (req, res) => {
-    try {
-        const stores = await Store.find();
-        res.status(200).json(stores);
+router.get('/:id', async (req, res) => storeController.getStoreById(req, res));
 
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
+router.put('/:id', async (req, res) => storeController.updateStore(req, res));
 
-router.get('/:id', async (req, res) => {
-    try {
-        const store = await Store.findById(req.params.id);
-
-        store
-        ? res.status(200).json(store)
-        : res.status(404).json({message: 'Store not found.'});
-
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
-
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedStore = await Store.findByIdAndUpdate(req.params.id, req.body, {new: true});
-
-        updatedStore
-        ? res.status(200).json(updatedStore)
-        : res.status(404).json({message: "Store not found."});
-
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const deletedStore = await Store.findByIdAndDelete(req.params.id);
-
-        deletedStore
-        ? res.status(200).json({message: "Store successfully deleted."})
-        : res.status(404).json({message: "Store not found."});
-
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-});
+router.delete('/:id', async (req, res) => storeController.deleteStore(req, res));
 
 module.exports = router;
